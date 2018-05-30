61627a46afe851a515e52056bb5c79034891e778
Carles <kirdie@gmail.com>
2018-05-30 15:25:57 +0400

    Added configuration file, Added targets capacity, Added few documentation clarifications

--- quick-load-incremental.sh
+++ quick-load-incremental.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-set -x
-RATE=$INITIAL
+SRC="conf.txt"
+TARGETS="targets.txt"
+if [ -f "$SRC" ]; then
+    source ./$SRC
+    echo "Loaded configuration from $SRC file."
+fi
+function attack(){
+    RATE=$INITIAL
+    echo "Initial rate is: $INITIAL"
+    while true; do
+      if [ -f $TARGETS ]; then
+        set -x
+        vegeta attack -targets=$TARGETS -duration $DURATION -rate $RATE | tee results.bin | vegeta report
+        set +x
+      else
+        set -x
+        echo "GET $URL" | vegeta attack -duration $DURATION -rate $RATE | tee results.bin | vegeta report
+        set +x
+      fi
+      echo "Waiting $SLEEP seconds"
+      sleep $SLEEP
+      RATE=$(($RATE+$INCREASE))
+      echo "Rate increased to: $RATE"
+    done
+}
+echo -e "Increase: $INCREASE\nWait time: $SLEEP"
 
-while true; do
-  echo "GET $URL" | vegeta attack -duration $DURATION -rate $RATE | tee results.bin | vegeta report
-  sleep $SLEEP
-  RATE=$(($RATE+$INCREASE))
-done
+attack
